86ffd21ff22255a513b9bdbcb2f3d912c0f851b9
homahi <homahi0128@gmail.com>
2018-02-05 14:11:48 +0900

    No293 自明な列挙体は省略して書くことが可能になる。

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -12,11 +12,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     @IBAction func launchCamera(_ sender: UIBarButtonItem) {
-        let camera = UIImagePickerControllerSourceType.camera
-        
-        if UIImagePickerController.isSourceTypeAvailable(camera){
+
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
             let picker = UIImagePickerController()
-            picker.sourceType = camera
+            picker.sourceType = .camera
             picker.delegate = self
             self.present(picker, animated: true)
         }
