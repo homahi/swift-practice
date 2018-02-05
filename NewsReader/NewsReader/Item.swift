11547ef925607b787cb098259659df5a4ea697e2
homahi <homahi0128@gmail.com>
2018-02-05 15:54:51 +0900

    No339 XMLパーサーでRSSリーダーをパース

---
+++ NewsReader/NewsReader/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by 原野誉大 on 2018/02/05.
+//  Copyright © 2018年 原野誉大. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    var link = ""
+}
